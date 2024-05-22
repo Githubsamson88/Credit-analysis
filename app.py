6f2c6a4424ed04ae1a746e85b0fafdd60b381370
Githubsamson88 <132224552+Githubsamson88@users.noreply.github.com>
2024-05-22 13:26:30 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -108,35 +108,7 @@ shap.waterfall_plot(shap_values_exp[0])
 
 st.pyplot()
 
-# Fonction pour tracer la courbe d'apprentissage
-def plot_learning_curve(estimator, X, y, cv=5):
-    train_sizes, train_scores, test_scores = learning_curve(
-        estimator, X, y, train_sizes=np.linspace(.1, 1.0, 5), cv=cv, scoring='accuracy', n_jobs=-1)
 
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-
-    plt.figure(figsize=(10, 6))
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1, color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
-    plt.xlabel("Training examples")
-    plt.ylabel("Score")
-    plt.legend(loc="best")
-    plt.title("Learning Curve")
-    plt.show()
-
-st.write("## Courbe d'apprentissage")
-plot_learning_curve(model, X, y, cv=5)
-st.pyplot(bbox_inches='tight')
-plt.clf()
 
 # Tracer la courbe ROC
 fpr, tpr, thresholds = roc_curve(y_test, y_probs)
