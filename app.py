31b1c55a334cfb6efdd2d9597d712459f9b534d3
Githubsamson88 <132224552+Githubsamson88@users.noreply.github.com>
2024-06-03 05:09:56 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -135,21 +135,6 @@ shap_values = explainer.shap_values(X_test)
 shap.initjs()
 shap.force_plot(explainer.expected_value, shap_values[0], X_test.iloc[0, :])
 
-# Courbe ROC
-fpr, tpr, _ = roc_curve(y_test, y_pred_proba_ngboost)
-roc_auc = auc(fpr, tpr)
-plt.figure(figsize=(8, 6))
-plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic (ROC) Curve')
-plt.legend(loc="lower right")
-plt.show()
-st.pyplot()
-
 
 ###
 # Visualisation avec SHAP
